eaa0c2ce5cc80293d2669d757aeedc3e5a2630e0
Russell Patterson <rwp.cpe@gmail.com>
2021-03-08 21:28:49 -0500

    App to waste Guids for all time.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace GuidBurner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var count = 0L;
+
+			while (true)
+			{
+				Console.WriteLine("{0}: {1}", ++count, Guid.NewGuid());
+			}
+		}
+	}
+}
